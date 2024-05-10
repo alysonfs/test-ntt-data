e780d2c5a2553e55b48e01b1726810c60016a829
Alyson F Souza <alysonforever@gmail.com>
2024-05-10 10:44:22 -0300

    feat: add class-validator to param validation

--- apps/backend/movie-api/src/main.ts
+++ apps/backend/movie-api/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(8080);
   const logger = new Logger('bootstrap');
   logger.log(`Listening on ${await app.getUrl()}`);
--- apps/backend/movie-api/src/search-movie/search-movie.controller.ts
+++ apps/backend/movie-api/src/search-movie/search-movie.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Param,  } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common'
+import { IsAlphanumeric, IsNotEmpty } from 'class-validator'
+
+export namespace DTO {
+  export namespace Input {
+    export class SearchMovie {
+      @IsNotEmpty()
+      @IsAlphanumeric()
+      name: string
+    }
+  }
+}
 
 @Controller('movie')
 export class SearchMovieController {
-  
   @Get('/search-movie/:name')
-  searchMovie(@Param('name') name: string): string {
-    return `Return: ${name}`;
+  searchMovie(@Param() params: DTO.Input.SearchMovie): string {
+    return `Return: ${params.name}`
   }
 }
