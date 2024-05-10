0d8a2d7cd37a4293b3f5bc14d84b46ef3a31a289
Alyson F Souza <alysonforever@gmail.com>
2024-05-10 10:08:24 -0300

    feat: Add HealthCheckController for health check endpoint

---
+++ apps/backend/movie-api/src/health-check.controller.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HealthCheckController } from './health-check.controller';
+
+describe('HealthCheckController', () => {
+  let controller: HealthCheckController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthCheckController],
+    }).compile();
+
+    controller = module.get<HealthCheckController>(HealthCheckController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+  
+  it('GET: /health-check - should returns OK', () => {
+    const result = controller.get();
+    expect(result).toBe("OK");
+  });
+});
