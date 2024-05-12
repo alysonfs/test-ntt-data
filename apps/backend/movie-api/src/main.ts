0f83dbf1bf9db313b1ce5f3c3a45f65f3ca0ae9a
Alyson F Souza <alysonforever@gmail.com>
2024-05-12 13:12:56 -0300

    chore(movie-api): enable app cors
    Ativando as políticas de CORS do Movie API

--- apps/backend/movie-api/src/main.ts
+++ apps/backend/movie-api/src/main.ts
@@ -5,12 +5,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({
     errorHttpStatusCode: 422,
   }));
   app.useGlobalInterceptors(new ResponseInterceptor());
-  
+
   await app.listen(8080);
   const logger = new Logger('bootstrap');
   logger.log(`Listening on ${await app.getUrl()}`);
